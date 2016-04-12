3bd56ba4d7e2d524cc6efe9aba56ff3a395011ed
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2016-04-12 16:30:36 +0100

    Finished initial implementation

--- usr/lib/pia-manager/pia-manager.py
+++ usr/lib/pia-manager/pia-manager.py
@@ -4,19 +4,55 @@ import sys, os
 import gettext
 import subprocess
 import gettext
+import uuid
+import time
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio
 
 
+
+
 # i18n
 gettext.install("pia-manager", "/usr/share/locale")
 
+CONFIGURATION = """[connection]
+id=PIA
+uuid=UUID
+type=vpn
+autoconnect=false
+permissions=user:LINUX_USERNAME:;
+secondaries=
+timestamp=TIMESTAMP
+
+[vpn]
+username=PIA_USERNAME
+comp-lzo=yes
+remote=PIA_GATEWAY
+connection-type=password
+password-flags=0
+ca=/usr/share/pia-manager/ca.crt
+service-type=org.freedesktop.NetworkManager.openvpn
+
+[vpn-secrets]
+password=PIA_PASSWORD
+
+[ipv4]
+dns-search=
+method=auto
+
+[ipv6]
+dns-search=
+ip6-privacy=0
+method=auto"""
+
+CONFIG_FILE = '/etc/NetworkManager/system-connections/PIA'
+
 class Manager(Gtk.Application):
     ''' Create the UI '''
-    def __init__(self):
-
+    def __init__(self, linux_username):
+        self.linux_username = linux_username
         Gtk.Application.__init__(self, application_id='com.pia.manager', flags=Gio.ApplicationFlags.FLAGS_NONE)
         self.connect("activate", self.on_activate)
 
@@ -38,6 +74,7 @@ class Manager(Gtk.Application):
         self.username = self.builder.get_object("entry_username")
         self.password = self.builder.get_object("entry_password")
         self.gateway = self.builder.get_object("combobox_gateway")
+        self.button = self.builder.get_object("button_ok")
 
         self.window.set_title("PIA")
         self.window.set_icon_name("pia-manager")
@@ -46,12 +83,9 @@ class Manager(Gtk.Application):
         self.builder.get_object("label_username").set_text(_("PIA username"))
         self.builder.get_object("label_password").set_text(_("PIA password"))
         self.builder.get_object("label_gateway").set_text(_("Gateway"))        
-        self.builder.get_object("checkbutton_show_password").set_label(_("Show password"))
+        self.builder.get_object("checkbutton_show_password").set_label(_("Show password"))        
 
-        self.builder.get_object("checkbutton_show_password").connect("toggled", self.quit)
-        self.builder.get_object("button_cancel").connect("clicked", self.on_quit)
-
-        (username, password, gateway) = self.read_configuration()
+        (username, password, self.gateway_value) = self.read_configuration()
         self.username.set_text(username)
         self.password.set_text(password)
 
@@ -67,7 +101,7 @@ class Manager(Gtk.Application):
                         gateway_id = bits[0]
                         gateway_name = " ".join(bits[1:])
                         iter = model.append([gateway_id, gateway_name])
-                        if gateway_id == gateway:
+                        if gateway_id == self.gateway_value:
                             selected_iter = iter
 
         self.gateway.set_model(model)
@@ -83,8 +117,24 @@ class Manager(Gtk.Application):
 
         self.add_window(self.window)
 
+        # Signals
+        self.builder.get_object("checkbutton_show_password").connect("toggled", self.on_show_password)
+        self.builder.get_object("button_cancel").connect("clicked", self.on_quit)
+        self.username.connect("changed", self.check_entries)
+        self.password.connect("changed", self.check_entries)
+        self.gateway.connect("changed", self.on_combo_changed)
+        self.button.connect("clicked", self.save_configuration)
+
     def on_show_password(self, checkbox):
         self.password.set_visibility(checkbox.get_active())
+
+    def on_combo_changed(self, combo):
+        tree_iter = combo.get_active_iter()
+        if tree_iter != None:
+            model = combo.get_model()
+            gateway_id, gateway_name = model[tree_iter][:2]
+            self.gateway_value = gateway_id
+            self.check_entries()        
 
     def on_quit(self, button):
         self.quit()
@@ -94,8 +144,8 @@ class Manager(Gtk.Application):
         password = ""
         gateway = None
         try:
-            if os.path.exists('/etc/NetworkManager/system-connections/PIA'):
-                with open('/etc/NetworkManager/system-connections/PIA') as fp:
+            if os.path.exists(CONFIG_FILE):
+                with open(CONFIG_FILE) as fp:
                     for line in fp:
                         line = line.strip()
                         if not line.startswith("#"):
@@ -111,7 +161,27 @@ class Manager(Gtk.Application):
             pass # best
         return (username, password, gateway)
 
+    def save_configuration(self, button):
+        configuration = CONFIGURATION.replace("PIA_USERNAME", self.username.get_text())
+        configuration = configuration.replace("PIA_PASSWORD", self.password.get_text())
+        configuration = configuration.replace("PIA_GATEWAY", self.gateway_value)
+        configuration = configuration.replace("LINUX_USERNAME", self.linux_username)
+        configuration = configuration.replace("UUID", str(uuid.uuid4()))
+        configuration = configuration.replace("TIMESTAMP", str(int(time.time())))
+        with open(CONFIG_FILE, 'w') as fp:
+            fp.writelines(configuration)
+        os.system("chmod 600 %s" % CONFIG_FILE)
+        os.system("service network-manager restart")
+        self.button.set_sensitive(False)
+
+    def check_entries(self, widget=None):
+        if (self.username.get_text() != "" and self.password.get_text() != "" and self.gateway_value is not None):
+            self.button.set_sensitive(True)
+        else:
+            self.button.set_sensitive(False)
+
 
 if __name__ == "__main__":
-    app = Manager()
+    linux_username = sys.argv[1]
+    app = Manager(linux_username)
     app.run(None)
